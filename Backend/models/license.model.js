7daf69ba28114882f2d4d73ef00bd107cc76b99c
IT22357380 <IT22357380@my.sliit.lk>
2025-03-20 07:12:30 +0530

    license model and route

---
+++ Backend/models/license.model.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const LicenseSchema = new mongoose.Schema(
+  {
+    licenseName: { type: String, required: true, trim: true },
+    title: { type: String, required: true, trim: true },
+    description: { type: String, required: true, trim: true },
+    price: { type: Number, required: true, min: 0 },
+    isTrial: { type: Boolean, required: true, default: false },
+  },
+);
+
+const License = mongoose.model("License", LicenseSchema);
+module.exports = License;
